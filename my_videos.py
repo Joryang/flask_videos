7bb4487b9bfd90e2735e51c2334c101b5d59de78
zhao <45147851@qq.com>
2016-02-29 18:28:44 +0800

    rel

---
+++ my_videos.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+from flask.ext.script import Manager
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask.ext.bootstrap import Bootstrap
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from werkzeug import secure_filename
+from flask.ext.sqlalchemy import SQLAlchemy
+import time
+import random
+import os
+
+class VideosUploadForm(Form):
+    video_name = FileField('Upload video')
+    submit = SubmitField('Submit')
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'hard to guess'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+bootstrap = Bootstrap(app)
+manager = Manager(app)
+db = SQLAlchemy(app)
+
+class Videos(db.Model):
+    __tablename__ = 'videos'
+    id = db.Column(db.Integer, primary_key=True)
+    video_name = db.Column(db.String(128), unique=True)
+
+    def __repr__(self):
+	return '<Videos %r>' % self.video_name
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = VideosUploadForm()
+    if form.validate_on_submit():
+	video_name = secure_filename(form.video_name.data.filename)
+	form.video_name.data.save('/var/www/wsgi-scripts/flask_videos/'+'flask'+str(int(time.time()*random.randint(1,100)))+video_name)
+	form.video_name.data = ''
+    return render_template('index.html', form=form)
+
+@app.route('/videos/')
+def videos():
+    return render_template('videos.html')
+
+if __name__ == '__main__':
+    manager.run()
